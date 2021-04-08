14c2c4d0df022bb73eb0ae5b1499bac2b957f65a
Raphaël Gilles <rgilles@student.42.fr>
2021-04-08 22:28:28 +0200

    need to do strdup

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,18 +1,31 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_strcpy.s                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/04/06 21:20:42 by rgilles           #+#    #+#              #
+#    Updated: 2021/04/06 21:20:43 by rgilles          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 global ft_strcpy
 
 section .text
 
 ft_strcpy:
-	mov		rax, rsi
-	mov		rbx, 0
+	mov		rax, rdi
 
 loop:
-	cmp		byte [rdi + rbx], 0
+	cmp		byte [rsi], 0
 	je		return
-	movzx	rcx, byte [rdi + rbx]
-	mov		[rsi + rbx], rcx
-	add		rbx, 1
-	jmp	loop
+	mov		al, byte [rsi]
+	mov		[rdi], al
+	inc		rsi
+	inc		rdi
+	jmp		loop
 
 return:
+
 	ret
--- main.c
+++ main.c
@@ -18,13 +18,13 @@ int		ft_strlen(char* str);
 int		ft_write(int fd, char *buf, int len);
 int		ft_read(int fd, char *buf, int len);
 int		ft_strcmp(char *str1, char *str2);	
-char	*ft_strcpy(char *orig, char *dest);
+char	*ft_strcpy(char *dest, char *orig);
 
 int main(void)
 {
 	char	*str;
 	char	*str2;
-	char	*read = calloc(10, 1);
+	char	*read;
 	int		ret;
 
 	str = "Coucou\n";
@@ -38,7 +38,9 @@ int main(void)
 	printf("Comp of \"\" and \"Coukou\\n\" is %d\n", ft_strcmp("", str2));
 	printf("Comp of \"Coukou\\n\" and \"\" is %d\n-------------------\n", ft_strcmp(str2, ""));
 
-	printf("Result of ft_strcpy(\"Coucou\\n\", str2) :%s-----------------\n", ft_strcpy(str, str2));
+	read = calloc(10, 1);
+	printf("Result of ft_strcpy(read, \"Coucou\\n\") :%s-----------------\n", ft_strcpy(read, str));
+	free(read);
 
 	printf("Return value of ft_write(1, \"Coucou\\n\", 7) is %d\n", ft_write(1, str, 7));
 	perror("The sys_write syscall encountered");
@@ -46,9 +48,10 @@ int main(void)
 	perror("The sys_write syscall encountered");
 
 	errno = 0;
+	read = calloc(10, 1);
 	ft_write(1, "-----------------\n", 18);
-	ft_write(1, "Reading 9 characters from stdin :", 33);
-	ret = ft_read(0, read, 5);
+	ft_write(1, "Reading 9 characters from stdin (8 + newline):", 46);
+	ret = ft_read(0, read, 9);
 	printf("\nReturned value is %d and edited buffer is now %s", ret, read);
 	perror("The sys_read syscall encountered");
 	ft_write(1, "\nReading from invalid input :", 29);
